0f7c8f64c53ad4af9c020a3d4b15dfbbadd49ef4
cjiho0312 <cjiho0312@naver.com>
2025-07-09 15:32:46 +0900

    7.9 이중 연결 리스트

---
+++ Program/Class 1th (Singgle linked list)/source.cpp
@@ -0,0 +1,208 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template <typename T>
+class List
+{
+private:
+    int listsize;
+
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* head;
+
+public:
+    List()
+    {
+        listsize = 0;
+        head = nullptr;
+    }
+
+    void push_front(T data)
+    {
+        Node* newnode = new Node;
+        newnode->data = data;
+
+        if (head == nullptr)
+        {
+            head = newnode;
+            newnode->next = nullptr;
+        }
+        else
+        {
+            newnode->next = head;
+            head = newnode;
+        }
+
+        listsize++;
+    }
+
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+        newNode->data = data;
+        newNode->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->next = newNode;
+        }
+
+
+        listsize++;
+
+    }
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "삭제할 데이터가 없습니다." << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            head = deleteNode->next;
+
+            delete deleteNode;
+
+            listsize--;
+        }
+    }
+
+    void pop_back()
+    {
+        if (head == nullptr)
+        {
+            cout << "삭제할 데이터가 없습니다." << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+            Node* previousNode = nullptr;
+
+            if (head->next == nullptr)
+            {
+                delete head;
+                head = nullptr;
+            }
+            else
+            {
+                while (deleteNode->next != nullptr)
+                {
+                    previousNode = deleteNode;
+                    deleteNode = deleteNode->next;
+                }
+
+                previousNode->next = deleteNode->next;
+                delete deleteNode;
+            }
+
+            listsize--;
+        }
+    }
+
+    void Remove(T data)
+    {
+        if (head == nullptr)
+        {
+            return;
+        }
+        else
+        {
+            Node* deleteNode = head;
+            Node* previousNode = head;
+
+            while (deleteNode != nullptr)
+            {
+                if (deleteNode->data == data)
+                {
+                    if (deleteNode == head)
+                    {
+                        head = deleteNode->next;
+                    }
+                    else
+                    {
+                        previousNode->next = deleteNode->next;
+                    }
+
+                    delete deleteNode;
+
+                    listsize--;
+
+                    deleteNode = head;
+                }
+
+                previousNode = deleteNode;
+
+                deleteNode = deleteNode->next;
+            }
+        }
+    }
+
+    bool empty()
+    {
+        // if (listsize == 0)
+        // {
+        //     return true;
+        // }
+        // else if (listsize != 0)
+        // {
+        //     return false;
+        // }
+
+        return (head == nullptr);
+    }
+
+    ~List()
+    {
+        while (head != nullptr)
+        {
+            pop_front();
+        }
+
+        if (listsize == 0)
+        {
+            cout << "Deleted all data" << endl;
+        }
+    }
+};
+
+int main()
+{
+    List<int> list;
+
+    cout << list.empty() << endl;
+
+    list.push_front(10);
+    list.push_front(20);
+    list.push_front(30);
+
+    list.push_back(5);
+    list.push_back(15);
+
+    list.Remove(20);
+
+    cout << list.empty() << endl;
+
+
+    return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -5,167 +5,31 @@ using namespace std;
 template <typename T>
 class List
 {
-private :
-    int listsize;
-
+private:
+    int size;
+    
     struct Node
     {
         T data;
         Node* next;
+        Node* previous;
     };
 
     Node* head;
+    Node* tail;
 
-public :
+public:
     List()
     {
-        listsize = 0;
+        size = 0;
         head = nullptr;
+        tail = nullptr;
     }
-
-    void push_front(T data)
-    {
-        Node* newnode = new Node;
-        newnode->data = data;
-
-        if (head == nullptr)
-        {
-            head = newnode;
-            newnode->next = nullptr;
-        }
-        else
-        {
-            newnode->next = head;
-            head = newnode;
-        }
-
-        listsize++;   
-    }
-
-
-    void push_back(T data)
-    {
-        Node* newNode = new Node;
-        newNode->data = data;
-        newNode->next = nullptr;
-        
-        if (head == nullptr)
-        {
-            head = newNode;
-        }
-        else
-        {
-            Node* currentNode = head;
-
-            while (currentNode->next != nullptr)
-            {
-                currentNode = currentNode->next;
-            }
-
-            currentNode->next = newNode;
-        }
-
-
-        listsize ++;
-
-    }
-
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "삭제할 데이터가 없습니다." << endl;
-        }
-        else
-        {
-            Node* deleteNode = head;
-            
-            head = deleteNode->next;
-
-            delete deleteNode;
-
-            listsize--;
-        }
-    }
-
-    void pop_back()
-    {
-        if (head == nullptr)
-        {
-            cout << "삭제할 데이터가 없습니다." << endl;
-        }
-        else
-        {
-            Node* deleteNode = head;
-            Node* previousNode = nullptr;
-
-            if (head->next == nullptr)
-            {
-                delete head;
-                head = nullptr;
-            }
-            else
-            {
-                while (deleteNode->next != nullptr)
-                {
-                    previousNode = deleteNode;
-                    deleteNode = deleteNode->next;
-                }
-            
-                previousNode->next = deleteNode->next;
-                delete deleteNode;
-            }
-
-            listsize--;
-        }
-    }
-
-    bool empty()
-    {
-        // if (listsize == 0)
-        // {
-        //     return true;
-        // }
-        // else if (listsize != 0)
-        // {
-        //     return false;
-        // }
-
-        return (head == nullptr);
-    }
-
-    ~List()
-    {
-        while (head != nullptr)
-        {
-            pop_front();
-        }
-
-        if (listsize == 0)
-        {
-            cout << "Deleted all data" << endl;
-        }
-    }
+    ~List();
 };
 
 int main()
 {
-    List<int> list;
-
-    cout << list.empty() << endl;
-
-    list.push_front(10);
-    list.push_front(20);
-    list.push_front(30);
-
-    list.push_back(5);
-    list.push_back(15);
-
-    list.pop_front();
-    list.pop_back();
-
-    cout << list.empty() << endl;
-    
 
     return 0;
 }
