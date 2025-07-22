fe1accd42b04e059d79d87c2cc8feeeadb84cbe3
cjiho0312 <cjiho0312@naver.com>
2025-07-22 15:44:11 +0900

    7.22 선형 큐

---
+++ Program/Class 6th (Stack)/source.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SIZE 5
+
+
+template <typename T>
+class Stack
+{
+private:
+    int count;
+    int highset;
+    T container[SIZE];
+
+public:
+    Stack()
+    {
+        count = 0;
+        highset = -1;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (count >= SIZE)
+        {
+            cout << "Stack overflow" << endl;
+        }
+        else
+        {
+            container[++highset] = data;
+            count++;
+        }
+    }
+
+    void pop()
+    {
+        if (count <= 0)
+        {
+            cout << "Stack Empty" << endl;
+        }
+        else
+        {
+            --highset;
+            --count;
+        }
+    }
+
+    const int& size()
+    {
+        return count;
+    }
+
+    const T& top()
+    {
+        return container[highset];
+    }
+
+    bool empty()
+    {
+        return (count == 0);
+    }
+
+    void showStack()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            cout << "[" << i << "] : " << container[i] << endl;
+        }
+        cout << endl << endl;
+    }
+};
+
+int main()
+{
+    Stack<int> stack;
+
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+    stack.push(40);
+    stack.push(50);
+
+    stack.pop();
+    stack.pop();
+
+    stack.showStack();
+
+    cout << "Empty : " << stack.empty() << endl;
+    cout << "Size : " << stack.size() << endl;
+    cout << "Top data : " << stack.top() << endl;
+
+    return 0;
+}
---
+++ Program/Class 7th (Linear Queue)/source.cpp
@@ -0,0 +1,93 @@
+﻿#include <iostream>
+
+#define SIZE 5
+
+using namespace std;
+
+template <typename T>
+class Queue
+{
+private:
+    int front;
+    int rear;
+    T container[SIZE];
+
+public:
+    Queue()
+    {
+        front = 0;
+        rear = 0;
+
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = NULL;
+        }
+    }
+
+    void push(T data)
+    {
+        if (rear >= SIZE)
+        {
+            cout << "Queue overflow" << endl;
+        }
+        else
+        {
+            container[rear++] = data;
+        }
+    }
+
+    void pop()
+    {
+        if (empty())
+        {
+            cout << "Queue Empty" << endl;
+        }
+        else
+        {
+            container[front++] = NULL;
+        }
+    }
+
+    const T& Front()
+    {
+        return front;
+    }
+
+    const int& size()
+    {
+        return rear - front;
+    }
+
+    void showQueue()
+    {
+        for (int i = front; i < rear; i++)
+        {
+            cout << container[i] << " ";
+        }
+        cout << endl;
+    }
+
+    bool empty()
+    {
+        return (front == rear);
+    }
+};
+
+int main()
+{
+    Queue<int> queue;
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    queue.push(40);
+
+    queue.pop();
+
+    queue.showQueue();
+
+    cout << "Front : " << queue.Front() << endl;
+    cout << "Size : " << queue.size() << endl;
+
+    return 0;
+}
